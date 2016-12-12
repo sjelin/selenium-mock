a711c840b24f49a47ca8b1c9d62c1420bc1f2902
Sammy Jelin <sjelin@gmail.com>
2016-12-12 11:41:35 -0800

    chore(dependencies): prefer `uuid` to `node-uuid`

--- lib/default-commands.ts
+++ lib/default-commands.ts
@@ -1,5 +1,5 @@
-import {v4 as uuid} from 'node-uuid';
 import * as os from 'os';
+import {v4 as uuid} from 'uuid';
 
 import {Command, GlobalCommand} from './command';
 import {Session} from './wd-interfaces';
